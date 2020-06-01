4867e852f3e9d5c16463b473f20ee15ce963d376
pastoikos <paulstoikos@gmail.com>
2020-06-01 19:16:59 +0300

    First commit

--- HLS/bilateralFilterKernel_HLS/bilateralFilterKernel.cpp
+++ HLS/bilateralFilterKernel_HLS/bilateralFilterKernel.cpp
@@ -2,13 +2,7 @@
 #include <kernels.h>
 #include <string.h>
 
-inline
-float exp1(float x) {
-  x = 1.0 + x / 256.0;
-  x *= x; x *= x; x *= x; x *= x;
-  x *= x; x *= x; x *= x; x *= x;
-  return x;
-}
+
 
 void bilateralFilterKernel(float* out, float* pad_depth, uint size_x,uint size_y,
 		const float * gaussian, float e_d, int r,int start,int end) {
@@ -28,53 +22,42 @@ void bilateralFilterKernel(float* out, float* pad_depth, uint size_x,uint size_y
 	uint y;
 	int depthsize_x=size_x+4; //324
 	float e_d_squared_2 = e_d * e_d * 2;
-	float out_array[320];
-	//float pad_depth_array[324*5];
-	float pad_depth_array_1[324];
-//#pragma HLS ARRAY_PARTITION variable=pad_depth_array_1 complete dim=1
-	float pad_depth_array_2[324];
-//#pragma HLS ARRAY_PARTITION variable=pad_depth_array_2 complete dim=1
-	float pad_depth_array_3[324];
-//#pragma HLS ARRAY_PARTITION variable=pad_depth_array_3 complete dim=1
-	float pad_depth_array_4[324];
-//#pragma HLS ARRAY_PARTITION variable=pad_depth_array_4 complete dim=1
-	float pad_depth_array_5[324];
-//#pragma HLS ARRAY_PARTITION variable=pad_depth_array_5 complete dim=1
+	float out_array[size_x];
+	float pad_depth_array_1[depthsize_x];
+	float pad_depth_array_2[depthsize_x];
+	float pad_depth_array_3[depthsize_x];
+	float pad_depth_array_4[depthsize_x];
+	float pad_depth_array_5[depthsize_x];
 
-	float gaussian_array[5];
+	float gaussian_array[5]; /* radius * 2 + 1 , const radius = 2 */
 	memcpy(gaussian_array,gaussian ,5*sizeof(float));
 
 
 
 	for (y = start; y < end; y++) {
 		uint pos = y * size_x;
-	//	memcpy(pad_depth_array,pad_depth+(y*depthsize_x),324*5*sizeof(float));
 
-		memcpy(pad_depth_array_1,pad_depth+(y*depthsize_x),324*sizeof(float));
-		memcpy(pad_depth_array_2,pad_depth+(y*depthsize_x)+(324*1),324*sizeof(float));
-		memcpy(pad_depth_array_3,pad_depth+(y*depthsize_x)+(324*2),324*sizeof(float));
-		memcpy(pad_depth_array_4,pad_depth+(y*depthsize_x)+(324*3),324*sizeof(float));
-		memcpy(pad_depth_array_5,pad_depth+(y*depthsize_x)+(324*4),324*sizeof(float));
+		memcpy(pad_depth_array_1,pad_depth+(y*depthsize_x),depthsize_x*sizeof(float));
+		memcpy(pad_depth_array_2,pad_depth+(y*depthsize_x)+(depthsize_x*1),depthsize_x*sizeof(float));
+		memcpy(pad_depth_array_3,pad_depth+(y*depthsize_x)+(depthsize_x*2),depthsize_x*sizeof(float));
+		memcpy(pad_depth_array_4,pad_depth+(y*depthsize_x)+(depthsize_x*3),depthsize_x*sizeof(float));
+		memcpy(pad_depth_array_5,pad_depth+(y*depthsize_x)+(depthsize_x*4),depthsize_x*sizeof(float));
 
-		for (uint x = 0; x < 320; x++) {
+		for (uint x = 0; x < size_x; x++) {
 
 #pragma HLS PIPELINE II=1
 
-			/*if (pad_depth_array[x+2+(2*depthsize_x)] == 0) {
-					out_array[x] = 0;
-					continue;
-			}*/
+		
 
 			float sum = 0.0f;
 			float t = 0.0f;
 
 
 			const float center = pad_depth_array_3[x+2];
-			//const float center = pad_depth_array[x+2+(2*depthsize_x)];
 
 
-			for (int i = -2; i <= 2; ++i) {
-				//for (int j = -2; j <= 2; ++j) {
+			for (int i = -r; i <= r; ++i) {
+
 					uint2 curPos = make_uint2(x + i+2,0);
 					float curPix = pad_depth_array_1[curPos.x];
 
@@ -135,7 +118,7 @@ void bilateralFilterKernel(float* out, float* pad_depth, uint size_x,uint size_y
 						sum += factor;
 
 					}
-				//}
+				
 			}
 
 			out_array[x] = t / sum;
